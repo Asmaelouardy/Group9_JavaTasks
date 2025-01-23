d00eac4237adbdf209df2dd5015a8c909f4f83cd
Asma <elouardyasma@gmail.com>
2025-01-22 21:30:25 -0500

    FinraTask solution modification added

--- src/Asma/week02/FINRATaskSolution.java
+++ src/Asma/week02/FINRATaskSolution.java
@@ -16,12 +16,12 @@ public static void FINRATask(){
 
     for (int num = 1; num <=30 ; num++) {
 
-        if(num % 3 ==0){
+        if(num % 3 == 0 && num % 5 == 0) {
+            System.out.print("FINRA ");
+        }else if(num % 3 ==0){
             System.out.print("FIN ");
         }else if(num % 5 == 0){
             System.out.print("RA ");
-        }else if(num % 3 == 0 && num % 5 == 0){
-            System.out.print("FINRA ");
         }else{
             System.out.print(num + " ");
         }
