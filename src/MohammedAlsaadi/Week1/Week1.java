6e3f9300362342abc3bfbd2fb3ad88db41a7760d
mohammed <alsaadimohammed1221@gmail.com>
2025-01-26 16:46:44 -0500

    Week one Task complete

---
+++ src/MohammedAlsaadi/Week1/Week1.java
@@ -0,0 +1,17 @@
+package MohammedAlsaadi.Week1;
+
+public class Week1 {
+    public class Week01 {public static void identifyOddEven(int number) {
+        if (number % 2 == 0) {
+            System.out.println(number + " is even");
+        } else {
+            System.out.println(number + " is odd");
+        }
+    }
+
+        public static void main(String[] args) {
+            identifyOddEven(5);  // Output: 5 is odd
+            identifyOddEven(6);  // Output: 6 is even
+        }
+    }
+}
