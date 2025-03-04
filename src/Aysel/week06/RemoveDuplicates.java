ba60bbfb1f6fafda9c6ff9df60c72e0526e94b2a
aysel <ayselarmy@hotmail.com>
2025-03-04 07:57:13 -0500

    Week6 Task Solution

---
+++ src/Aysel/week06/RemoveDuplicates.java
@@ -0,0 +1,39 @@
+package Aysel.week06;
+
+public class RemoveDuplicates {
+
+    public static void main(String[] args) {
+        /*
+        String--Remove Duplicates
+        Write a return method that can remove the duplicated values
+        from String
+         */
+        System.out.println(removeDup("AAABBBCCC"));
+        System.out.println(removeDup("AABBCCDD"));
+        System.out.println(removeDup("Hello World!"));
+        System.out.println(removeDup("CCCBBBAAA"));
+    }
+
+
+
+        public static String removeDup(String str) {
+            String result = "";
+            for (int i = 0; i < str.length(); i++) {
+                if (!result.contains(String.valueOf(str.charAt(i)))) {
+                    result += str.charAt(i);
+                }
+            }
+            return result;
+        }
+
+
+
+    }
+
+
+
+
+
+
+
+
