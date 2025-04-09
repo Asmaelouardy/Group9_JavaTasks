b954f196d8dcddcd347e99619b7850467c0f20a5
aysel <ayselarmy@hotmail.com>
2025-04-09 08:36:14 -0400

    Task 09 Solution

--- src/Aysel/week09/Array_SortAscending.java
+++ src/Aysel/week09/Array_SortAscending.java
@@ -4,31 +4,37 @@ import java.util.Arrays;
 
 public class Array_SortAscending {
 
-    public static void main(String[] args) {
-
-
-//Write a return method that can sort an int array in Ascending order
+    //Write a return method that can sort an int array in Ascending order
         // without using the sort method of the Arrays class
 
+    public static void main(String[] args) {
+                int[] array = {64, 25, 12, 22, 11};
 
-        int[] arr = {7,8,9,10};
-        int[] result = reverseArray(arr);
+                array = arraySort(array);
+                System.out.println("Sorted array:");
 
-        System.out.println(Arrays.toString(result));
-    }
-    public static int[] reverseArray(int[] array) {
-        if (array.length <= 1) {
-            return array;
+                for (int num : array) {
+                    System.out.print(num + " ");
+                }
+            }
+
+            public static int[] arraySort(int[] array) {
+                for (int i = 0; i < array.length - 1; i++) {
+                    int minIndex = i;
+                    for (int j = i + 1; j < array.length; j++) {
+                        if (array[j] < array[minIndex]) {
+                            minIndex = j;
+                        }
+                    }
+
+                    int temp = array[i];
+                    array[i] = array[minIndex];
+                    array[minIndex] = temp;
+                }
+                return array;
+            }
         }
-        int[] reversedArray = new int[array.length];
 
-        for (int i = array.length - 1, j = 0; i >= 0; i--, j++) {
-            reversedArray[j] = array[i];
-        }
-        return reversedArray;
-    }
-
-}
 
 
 
